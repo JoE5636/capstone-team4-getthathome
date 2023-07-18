64e1785b27c4bff84e3a23d18d455ae34274b640
JoE5636 <joeldelarosalopez@gmail.com>
2023-07-17 18:47:19 -0600

    feature: header done, set typography, button modified

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,24 +1,56 @@
 import styled from "@emotion/styled";
 import homeLogo from "../assets/Logo.png";
+import { AiOutlineUserAdd } from "react-icons/ai";
+import { BiSearch } from "react-icons/bi";
+import { colors } from "../styles";
 import Button from "./button";
 
 const Wrapper = styled.header`
   display: flex;
+  box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.2);
+  padding: 5px 50px;
+`;
+
+const Nav = styled.div`
+  width: 100%;
+  display: flex;
   justify-content: space-between;
-  padding: 10px 32px;
+  padding: 5px 32px;
 `;
 
 function Header() {
   return (
     <Wrapper>
-      <div>
-        <img src={homeLogo}></img>
-      </div>
-      <div style={{ display: "flex", gap: "16px" }}>
-        <button>Find a home</button>
-        <button>Join</button>
-        <Button type="primary">Log in</Button>
-      </div>
+      <Nav>
+        <div>
+          <img src={homeLogo}></img>
+        </div>
+        <div style={{ display: "flex", gap: "16px" }}>
+          <Button style={{ backgroundColor: `${colors.white}` }}>
+            <BiSearch
+              style={{
+                width: "24px",
+                height: "24px",
+              }}
+            />
+            FIND A HOME
+          </Button>
+          <Button
+            rounded
+            style={{
+              border: `1px solid ${colors.pink[500]}`,
+              backgroundColor: `${colors.white}`,
+            }}
+          >
+            <AiOutlineUserAdd style={{ width: "24px", height: "24px" }} />
+            JOIN
+          </Button>
+          <Button type="primary" rounded>
+            <AiOutlineUserAdd style={{ width: "24px", height: "24px" }} />
+            LOGIN
+          </Button>
+        </div>
+      </Nav>
     </Wrapper>
   );
 }

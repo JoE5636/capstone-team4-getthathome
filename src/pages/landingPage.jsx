ec6ae99464ae809ae0825cb215e0481ba3cfd057
JoE5636 <joeldelarosalopez@gmail.com>
2023-07-26 15:10:46 -0600

    fix: favorites on seeker & properties on landingpage

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -13,52 +13,70 @@ import Depa from "../assets/depa1.png";
 
 const sampleData = [
   {
-    id: 1,
+    id: 13,
     operation: "Venta",
-    address: "Calle 123, Ciudad A",
+    address: "Calle Los Rosales 456, San Borja, Lima",
     category: "Department",
-    price: 250000,
-    maintenance: 200,
-    pets: true,
-    bedrooms: 3,
-    bathrooms: 2,
-    area: 150,
-    description: "Hermosa casa en venta con amplio jardín.",
-    created_at: "2023-07-21T14:15:54.448Z",
-    updated_at: "2023-07-21T14:15:54.562Z",
-    photos: { Depa },
-  },
-  {
-    id: 2,
-    operation: "Renta",
-    address: "Avenida XYZ, Ciudad B",
-    category: "Home",
-    price: 1200,
-    maintenance: 100,
+    price: 4049,
+    maintenance: 206,
     pets: false,
     bedrooms: 2,
-    bathrooms: 1,
-    area: 80,
-    description: "Apartamento en renta cerca del centro comercial.",
-    created_at: "2023-07-21T14:15:54.453Z",
-    updated_at: "2023-07-21T14:15:54.631Z",
-    photos: { Depa },
+    bathrooms: 3,
+    area: 121,
+    description: "Propiedad 13 con descripción aleatoria.",
+    created_at: "2023-07-26T19:48:06.414Z",
+    updated_at: "2023-07-26T19:48:06.465Z",
+    photos: [
+      "http://127.0.0.1:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBZ2tCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--04473d87183447e3888c1c8cfcd689418af28b54/img01.jpg",
+      "http://127.0.0.1:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBZ29CIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--841f2b91a75b3aa85c5bab25ed6a549b712c29ef/img04.jpg",
+      "http://127.0.0.1:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBZ3NCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--91810d9fdda8fec00ecd7ea8dd58a8d88e1b9a91/img08.jpg",
+      "http://127.0.0.1:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBZ3dCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--e264fddcf8b7450010b7cb14cf7e4ad6de894bee/img06.jpg",
+      "http://127.0.0.1:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBZzBCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--e0a02abaacc436398d97323e155d16f3c0ebdb1e/img10.jpg",
+    ],
   },
   {
-    id: 3,
+    id: 1,
     operation: "Venta",
-    address: "Calle Principal, Ciudad C",
+    address: "Av. Arequipa 123, San Isidro, Lima",
     category: "Home",
-    price: 180000,
-    maintenance: 150,
+    price: 2015,
+    maintenance: 103,
     pets: true,
-    bedrooms: 4,
-    bathrooms: 3,
-    area: 200,
-    description: "Casa en venta con piscina y vista al mar.",
-    created_at: "2023-07-21T14:15:54.458Z",
-    updated_at: "2023-07-21T14:15:54.687Z",
-    photos: { Depa },
+    bedrooms: 3,
+    bathrooms: 4,
+    area: 202,
+    description: "Propiedad 1 con descripción aleatoria.",
+    created_at: "2023-07-26T19:48:05.647Z",
+    updated_at: "2023-07-26T19:48:05.718Z",
+    photos: [
+      "http://127.0.0.1:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBYzA9IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--98f2852e6800789fa312f820b1c41d30c459d73d/img06.jpg",
+      "http://127.0.0.1:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBYzQ9IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--845e4e63e9d5fcfbf4cfee6a654274f768e43646/img14.jpg",
+      "http://127.0.0.1:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBYzg9IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--c90d6252e24c6199057d18f7c605d1a0c70da259/img17.jpg",
+      "http://127.0.0.1:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBZEE9IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--24bede55ee57eb724a33b3473146f8caa616eaa2/img09.jpg",
+      "http://127.0.0.1:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBZEU9IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--636d42962f29e7810bed2320a456f38fb812695a/img11.jpg",
+    ],
+  },
+  {
+    id: 38,
+    operation: "Venta",
+    address: "Calle Los Tulipanes 852, San Juan de Lurigancho, Lima",
+    category: "Department",
+    price: 4455,
+    maintenance: 86,
+    pets: false,
+    bedrooms: 1,
+    bathrooms: 2,
+    area: 314,
+    description: "Propiedad 38 con descripción aleatoria.",
+    created_at: "2023-07-26T19:48:07.802Z",
+    updated_at: "2023-07-26T19:48:07.854Z",
+    photos: [
+      "http://127.0.0.1:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBb1lCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--296f40493f7c32ff9a49aa8830f166bbdc7f5281/img04.jpg",
+      "http://127.0.0.1:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBb2NCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--d1ec42e55e8ecd429d7997d6e327e501ee6e8efd/img08.jpg",
+      "http://127.0.0.1:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBb2dCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--ca3c78580b02e71c6af8737f1cdcce11d68bc363/img18.jpg",
+      "http://127.0.0.1:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBb2tCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--3a62f86d4e702b1065ecee47e33555875beac794/img17.jpg",
+      "http://127.0.0.1:3000/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBb29CIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--e47fbd2489b8cc3554e076f598604cc333c4c563/img19.jpg",
+    ],
   },
 ];
 
@@ -301,6 +319,7 @@ function LandingPage() {
             <TitleProp>Homes for rent at the best prices</TitleProp>
             <PropertySample>
               <PropertyCard
+                id={sampleData[0].id}
                 operation={sampleData[0].operation}
                 photos={sampleData[0].photos[0]}
                 price={sampleData[0].price}
@@ -311,6 +330,7 @@ function LandingPage() {
                 pets={sampleData[0].pets}
               />
               <PropertyCard
+                id={sampleData[1].id}
                 operation={sampleData[1].operation}
                 photos={sampleData[1].photos[1]}
                 price={sampleData[1].price}
@@ -321,6 +341,7 @@ function LandingPage() {
                 pets={sampleData[1].pets}
               />
               <PropertyCard
+                id={sampleData[2].id}
                 operation={sampleData[2].operation}
                 photos={sampleData[2].photos[2]}
                 price={sampleData[2].price}
